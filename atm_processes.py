ffef934f09c071282c55e4db31ab677178a01e57
MothoF <mothofeelama@gmail.com>
2024-11-24 20:35:52 +0200

    Update atm_processes.py

--- atm_processes.py
+++ atm_processes.py
@@ -3,6 +3,8 @@ import random
 import time
 import platform
 import make_database
+import getpass
+from datetime import datetime
 
 digits = [0,1,2,3,4,5,6,7,8,9]
 
@@ -30,17 +32,18 @@ def generate_account_pin():
     return account_pin
 
 def change_account_pin(account_id):
-    new_pin = input("Enter your new 5 digit pin here: ")
+    new_pin = getpass.getpass("Enter your new 5 digit pin here: ")
     old_pin = make_database.select_account_pin(account_id)
     if new_pin != old_pin:
         if not new_pin.isdigit():
             print("Pin must be made of digits only")
             time.sleep(5)
             clear_terminal()
-        elif len(new_pin)<5:
-            print("Pin must be 5 digits long")
+        elif len(new_pin) != 5:
+            print("Pin must be exactly 5 digits long")
             time.sleep(5)
             clear_terminal()
+            menu(account_id)
         else:
             #print("You have successfully updated your pin")
             make_database.update_accounts_pin(new_pin,account_id)
@@ -70,12 +73,58 @@ def change_account_pin(account_id):
             clear_terminal()
             menu(account_id)
 
+def delete_profile(userID,accountID):
+    print("\nThis action will delete all of your information from our database, including your bank account")
+    choice = input("Are your certain you want to go ahead with this action?[Type Y for yes or N for No]: ")
+    if choice == 'Y' or choice == 'y':
+        make_database.delete_clients(userID)
+        make_database.delete_accounts_all(userID)
+        make_database.delete_transactions_single_account_all(accountID)
+        time.sleep(5)
+        clear_terminal()
+        print("\nDeleting profile...")
+        time.sleep(5)
+        clear_terminal()
+        print("\nYou have successfuly deleted your profile.")
+        time.sleep(5)
+        clear_terminal()
+    elif choice == 'N' or choice == 'n':
+        time.sleep(5)
+        clear_terminal()
+        menu(accountID)
+    else:
+        print(f"{choice} is an invalid choice. Type 'Y' for yes or 'N' for no")
+        time.sleep(5)
+        clear_terminal()
+        menu(accountID)
+
+def user_name_verification(username):
+    all_usernames_in_database = make_database.select_all_client_id()
+    if username in all_usernames_in_database:
+        return False
+    else:
+        return True
+
 def sign_up():
     credential_list = []
     id = input("Enter your username here: ")
     if id:
-        credential_list.append(id)
-        password = input("Enter your password here: ")
+        if user_name_verification(id) == True:
+            credential_list.append(id)
+            password = getpass.getpass("Enter your password here: ")
+        else:
+            print(f"\n{id} is already taken. Select a new username")
+            continue_or_not = input("Enter E to exit or any other button to return to the menu: ")
+            if continue_or_not == 'E' or continue_or_not == 'e':
+                time.sleep(3)
+                clear_terminal()
+                print("\nExiting program...")
+                time.sleep(5)
+                clear_terminal()
+            else:
+                time.sleep(3)
+                clear_terminal()
+                sign_up()
         if password:
             credential_list.append(password)
             first_name = input("Enter your first name here: ")
@@ -125,8 +174,23 @@ def sign_up():
 
 def sign_in():
     user_id = input("Enter your username here: ")
-    user_password = input("Enter your password here: ")
-    pass
+    user_password = getpass.getpass("Enter your password here: ")
+    if user_name_verification(user_id) == False and (user_password == make_database.select_client_password(user_id)):
+            account_id = make_database.select_account_id(user_id)
+            return account_id
+    else:
+        print("Username or password does not match database records")
+        continue_or_not = input("Enter E to exit or any other button to return to the landing page: ")
+        if continue_or_not == 'E' or continue_or_not == 'e':
+            time.sleep(3)
+            clear_terminal()
+            print("\nExiting program...")
+            time.sleep(5)
+            clear_terminal()
+        else:
+            time.sleep(3)
+            clear_terminal()
+            landing_screen()
 
 def landing_screen():
     print("Select the number associated with action you wish to complete:")
@@ -160,7 +224,8 @@ def make_deposit(account_id):
     transaction_amount = input("Enter the deposit amount here: ")
     if transaction_amount.isdigit():
         transaction_amount = int(transaction_amount)
-        make_database.insert_into_transactions(account_id,transaction_type,transaction_amount)
+        time_of_transaction = str(datetime.now())
+        make_database.insert_into_transactions(account_id,transaction_type,transaction_amount,time_of_transaction)
         make_database.update_accounts_balance_deposit(transaction_amount,account_id)
         new_balance = make_database.select_account_balance(account_id)
         print(f"\nYou have successfully made a R {transaction_amount} cash deposit.\nYour account balance is now {new_balance}")
@@ -182,9 +247,10 @@ def make_withdrawal(account_id):
     transaction_amount = input("Enter the withdrawal amount here: ")
     if transaction_amount.isdigit():
         transaction_amount = int(transaction_amount)
+        time_of_transaction = str(datetime.now())
         current_balance = make_database.select_account_balance(account_id)
         if transaction_amount <= current_balance:
-            make_database.insert_into_transactions(account_id,transaction_type,transaction_amount)
+            make_database.insert_into_transactions(account_id,transaction_type,transaction_amount,time_of_transaction)
             make_database.update_accounts_balance_withdrawal(transaction_amount,account_id)
             new_balance = make_database.select_account_balance(account_id)
             print(f"\nYou have successfully made a R {transaction_amount} cash withdrawal.\nYour account balance is now {new_balance}")
@@ -219,7 +285,7 @@ def make_withdrawal(account_id):
         make_deposit(account_id)
 
 def change_password(userID):
-    new_password = input("Enter your new profile password here: ")
+    new_password = getpass.getpass("Enter your new profile password here: ")
     old_password = make_database.select_client_password(userID)
     account_id = make_database.select_account_id(userID)
     if new_password != old_password:
@@ -259,8 +325,8 @@ def menu(account_id):
     print("4. Change account pin")
     print("5. change profile password")
     print("6. Delete profile")
-    print("7. Delete account")
-    print("8. Delete transaction history")
+    # print("7. Delete account")
+    # print("8. Delete transaction history")
     print("0. Exit the program")
     print("\n")
     choice = input("Enter the number corresponding with the action you would like to take here: ")
@@ -310,18 +376,21 @@ def menu_actions(choice,account_id):
     elif int(choice) == 6:
         time.sleep(3)
         clear_terminal()
-        pass
-    elif int(choice) == 7:
-        time.sleep(3)
-        clear_terminal()
-        pass
-    elif int(choice) == 8:
-        time.sleep(3)
-        clear_terminal()
-        pass
+        user_id = make_database.select_client_id(account_id)
+        delete_profile(user_id,account_id)
+    # elif int(choice) == 7:
+    #     time.sleep(3)
+    #     clear_terminal()
+    #     pass
+    # elif int(choice) == 8:
+    #     time.sleep(3)
+    #     clear_terminal()
+    #     pass
 
 def menu_backend_logic_layout():
-    if landing_screen() == '1':
+    make_database.build_db()
+    user_choice = landing_screen()
+    if user_choice == '1':
         details = sign_up()
         username = details[0]
         password = details[1]
@@ -332,7 +401,6 @@ def menu_backend_logic_layout():
         account_pin = details[6]
         account_type = 'personal account'
         account_balance = 0
-        make_database.build_db()
         continue_or_not = input("Enter E to exit or any other button to continue: ")
         if continue_or_not == 'E' or continue_or_not == 'e':
             make_database.insert_into_clients(username,password,name,surname,email)
@@ -351,11 +419,12 @@ def menu_backend_logic_layout():
             clear_terminal()
             menu(account_id)
 
-    elif landing_screen() == '2':
-        sign_in()
+    elif user_choice == '2':
+        account_id = sign_in()
         time.sleep(5)
         clear_terminal()
-        menu(account_id)
+        if account_id:
+            menu(account_id)
 
 menu_backend_logic_layout()
 
